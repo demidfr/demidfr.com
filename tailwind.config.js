1a40571c58d4dcaa7e5914cf7d17c0791795a740
GentooWizard <demidfr@proton.me>
2024-12-17 12:19:00 +0200

    Website overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,20 @@ module.exports = {
   content: ["./*.html", "./blog/*.html"],
   theme: {
     extend: {
+		backgroundImage: {
+			'pattern': "url('./image/webg.jpg')"
+		},
 		colors: {
-        'background': '#282828',
-		'foreground': '#ebdbb2',
-		'yellow': '#f0c674',
+        'background': '#0a1519',
+		'foreground': '#0acbe3',
+		'gradstep1': '#0e4351',
+		'gradstep2': '#142b44',
+		'gradstep3': '#15394C',
       },
 	  spacing: {
         '128': '32rem',
+		'220': '55rem',
+		'240': '60rem',
 		'256': '64rem',
       }
 	},
